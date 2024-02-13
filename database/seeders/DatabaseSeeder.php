04a95499cedcd29cfd62c6e0a5093422ba100ed5
User <j04719671@gmail.com>
2024-02-13 11:55:47 +0700

    v1.0.1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,6 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         // $this->run(GuruSeeder::class);
-        $this->run(MapelSeeder::class);
+        // $this->run(MapelSeeder::class);
     }
 }
---
+++ resources/views/guru/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layout.main')
+@section('konten')
+    <h1>Guru</h1>
+    <table class="table table-info table-striped ">
+        <thead>
+          <tr>
+            <th scope="col">No</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </table>
+@endsection
